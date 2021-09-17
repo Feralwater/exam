316a997b5b4f3e89b9b3ced7420bb255e3f9dbfb
Feralwater <lineviktoriya@gmail.com>
2021-09-17 21:25:40 +0300

    implement counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Button from "./button/Button";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    const [count, setCount] = useState<number>(0)
+    const incrementCount = () => {
+        setCount(prev => prev + 1)
+    }
+    const resetCount = () => {
+        setCount(0)
+    }
+    return (
+        <div className="App">
+            <div>
+                <div>
+                    <div>{count}</div>
+                </div>
+                <div>
+                    <Button onClick={incrementCount}>inc</Button>
+                    <Button onClick={resetCount}>reset</Button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/button/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type PropsButtonType = {
+    onClick: () => void
+}
+const Button: React.FC<PropsButtonType> = ({children, onClick}) => {
+    return (
+        <button onClick={onClick}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
