cc1ebe5663b672d11563a1d8a7b3bdaec905f436
Feralwater <lineviktoriya@gmail.com>
2021-10-01 20:29:51 +0300

    Add safety from incorrect input

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,25 @@ import s from './App.module.css';
 import Button from "./button/Button";
 import SetValue from "./setValue/SetValue";
 
-let MAXIMUM: string = "0";
-let MINIMUM: string = "0";
+let MAXIMUM: number = 1;
+let MINIMUM: number = 0;
 let disableSet: boolean = false;
 
 function App() {
     const [count, setCount] = useState<number>(0)
-    const [minimum, setMinimum] = useState<string>("")
-    const [maximum, setMaximum] = useState<string>("")
+    const [minimum, setMinimum] = useState<number>(0)
+    const [maximum, setMaximum] = useState<number>(1)
     const incrementCount = () => {
         setCount(prev => prev + 1)
     }
     const resetCount = () => {
         setCount(0)
     }
-    const setMaxValue = (value: string) => {
+    const setMaxValue = (value: number) => {
         setMaximum(value);
         disableSet = false;
     }
-    const setStartValue = (value: string) => {
+    const setStartValue = (value: number) => {
         setMinimum(value);
         disableSet = false;
     }
@@ -29,7 +29,7 @@ function App() {
     const setValue = () => {
         MAXIMUM = maximum;
         MINIMUM = minimum;
-        setCount(+minimum);
+        setCount(minimum);
         disableSet = true;
         return {
             MAXIMUM,
@@ -39,24 +39,36 @@ function App() {
 
     return (
         <div className={s.App}>
-
             <div className={s.counter_container}>
                 <div className={s.display}>
-                    <SetValue onChange={setMaxValue} value={maximum}>max value</SetValue>
-                    <SetValue onChange={setStartValue} value={minimum}>start value</SetValue>
+                    <SetValue onChange={setMaxValue}
+                              value={maximum}
+                              className={+minimum >= maximum ? "redInput" : "input"}
+                    >max value</SetValue>
+                    <SetValue onChange={setStartValue}
+                              value={minimum}
+                              className={minimum >= maximum ? "redInput" : "input"}
+                    >start value</SetValue>
                 </div>
                 <div className={s.buttons_container}>
-                    <Button onClick={setValue} disabled={disableSet}>set</Button>
+                    <Button onClick={setValue}
+                            disabled={disableSet || minimum >= maximum}
+                    >set</Button>
                 </div>
             </div>
 
             <div className={s.counter_container}>
                 <div className={s.display}>
-                    <div className={count === +MAXIMUM && count > 0 ? s.counter + " " + s.red : s.counter}>{count}</div>
+                    <div className={count === MAXIMUM && count > 0 ? s.counter + " " + s.red : s.counter}
+                    >{count}</div>
                 </div>
                 <div className={s.buttons_container}>
-                    <Button onClick={incrementCount} disabled={count === +MAXIMUM}>inc</Button>
-                    <Button onClick={resetCount} disabled={count === +MINIMUM}>reset</Button>
+                    <Button onClick={incrementCount}
+                            disabled={count === MAXIMUM}
+                    >inc</Button>
+                    <Button onClick={resetCount}
+                            disabled={count === MINIMUM}
+                    >reset</Button>
                 </div>
             </div>
         </div>
--- src/setValue/SetValue.tsx
+++ src/setValue/SetValue.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import s from "./SetValue.module.css"
 
 type PropsInputType = {
-    onChange: (value: string) => void
-    value: string
+    onChange: (value: number) => void
+    value: number
+    className: string
 }
 
-const SetValue: React.FC<PropsInputType> = ({children, value, onChange}) => {
+const SetValue: React.FC<PropsInputType> = (
+    {children, value, onChange, className}
+) => {
     return (
         <div className={s.container}>
             <div className={s.text}>{children}:</div>
             <input
-                className={s.input}
+                className={s[className]}
                 type="number"
                 value={value}
-                onChange={(e) => onChange(e.currentTarget.value)}
+                onChange={(e) => onChange(+e.currentTarget.value)}
             />
         </div>
     );
